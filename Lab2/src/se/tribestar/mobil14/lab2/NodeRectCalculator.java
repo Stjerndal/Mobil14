842b773ee1d12ac02e66c66bfa4e4379f0be9a25
Stjerndal <stjernd@kth.se>
2014-12-02 00:25:24 +0100

    Added class for calculating node rect bounds.

---
+++ Lab2/src/se/tribestar/mobil14/lab2/NodeRectCalculator.java
@@ -0,0 +1,31 @@
+package se.tribestar.mobil14.lab2;
+
+import java.util.Arrays;
+
+import android.graphics.Rect;
+
+public class NodeRectCalculator {
+
+	private static final int[] realIndices = { 0, 3, 6, 8, 10, 12, 16, 17, 18, 21, 22, 23, 25, 26, 27, 30, 31, 32, 36,
+			38, 40, 42, 45, 48 }; // awesome hardcodning
+
+	public static Node[] getNodes(int x, int y, int width, int height) {
+		Node[] nodes = new Node[24];
+		int rectSize = width / 7;
+		int indx = 0; // 0-48
+		int nodeIndx = 0; // 0-23
+		for (int i = 0; i < 7; ++i) {
+			int left = x + i * rectSize;
+			int right = left + rectSize;
+			for (int j = 0; j < 7; ++j, ++indx) {
+				if (Arrays.asList(realIndices).contains(indx)) {
+					int top = y + j * rectSize;
+					int bottom = top + rectSize;
+					Rect rect = new Rect(left, top, right, bottom);
+					nodes[nodeIndx] = new Node(rect);
+				}
+			}
+		}
+		return nodes;
+	}
+}
