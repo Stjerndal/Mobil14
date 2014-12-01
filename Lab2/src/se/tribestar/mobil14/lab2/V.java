cf2fe1e6a97cd42d42b1885a662231075a4d0e26
Stjerndal <stjernd@kth.se>
2014-12-01 22:02:06 +0100

    Created V file for logging etc.

---
+++ Lab2/src/se/tribestar/mobil14/lab2/V.java
@@ -0,0 +1,11 @@
+package se.tribestar.mobil14.lab2;
+
+import android.util.Log;
+
+public class V {
+	public final static String TAG = "LAB2";
+
+	public static void log(String msg) {
+		Log.d(TAG, msg);
+	}
+}
