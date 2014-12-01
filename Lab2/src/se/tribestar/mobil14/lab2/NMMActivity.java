7e461e09d93e5e5d5446e93815bb2f5d23f10287
modstam <modstam@live.com>
2014-12-01 21:37:45 +0100

    copy pasted main and NMNactivty

--- Lab2/src/se/tribestar/mobil14/lab2/NMMActivity.java
+++ Lab2/src/se/tribestar/mobil14/lab2/NMMActivity.java
@@ -1,5 +1,35 @@
 package se.tribestar.mobil14.lab2;
 
-public class NMMActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 
+public class NMMActivity extends Activity {
+
+	private NMMView view;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Get the dimensions of the display
+		DisplayMetrics metrics = new DisplayMetrics();
+		this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		// Create the view
+		view = new NMMView(this, metrics.widthPixels, metrics.heightPixels);
+		this.setContentView(view);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		view.resume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		view.pause();
+	}
 }
