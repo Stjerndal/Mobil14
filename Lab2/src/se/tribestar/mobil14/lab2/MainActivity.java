c67b5b671cea02ea047dbd9224089efa83ba89f5
modstam <modstam@live.com>
2014-12-01 21:38:52 +0100

    fixed error

--- Lab2/src/se/tribestar/mobil14/lab2/MainActivity.java
+++ Lab2/src/se/tribestar/mobil14/lab2/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.activity_main);
 	}
 
 	public void onStartClick(View view) {
