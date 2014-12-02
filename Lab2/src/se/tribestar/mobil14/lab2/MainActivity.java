6f2f1e3a16893f44207dd8f5f0ed5d7f1b8f86c9
Stjerndal <stjernd@kth.se>
2014-12-02 20:24:54 +0100

    Added options?

--- Lab2/src/se/tribestar/mobil14/lab2/MainActivity.java
+++ Lab2/src/se/tribestar/mobil14/lab2/MainActivity.java
@@ -3,6 +3,7 @@ package se.tribestar.mobil14.lab2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -20,5 +21,12 @@ public class MainActivity extends Activity {
 
 	public void onExitClick(View view) {
 		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
 	}
 }
