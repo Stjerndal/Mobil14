2521a42e387a7e1e1cc132cba6b54022e744d542
Stjerndal <stjernd@kth.se>
2014-12-03 20:07:40 +0100

    Sprite documentation.

--- Lab2/src/se/tribestar/mobil14/lab2/Sprite.java
+++ Lab2/src/se/tribestar/mobil14/lab2/Sprite.java
@@ -4,11 +4,12 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 
+//class to represent a sprite 
 public class Sprite {
-	private int posX, posY; // position
+	private int posX, posY; // pixel position
 	private Drawable sprite; // a representation of the icon image
 	private int screenWidth, screenHeight; // screen sizes
-	private int width, height;
+	private int width, height; // width and height in pixels
 
 	public Sprite(int posX, int posY, Drawable sprite, int screenWidth, int screenHeight) {
 		this.posX = posX;
@@ -20,6 +21,7 @@ public class Sprite {
 		this.height = sprite.getIntrinsicHeight();
 	}
 
+	// used for creating sprite of a size relative to screen size
 	public Sprite(int posX, int posY, Drawable sprite, int screenWidth, int screenHeight, float partOfScreenWidth) {
 		this.posX = posX;
 		this.posY = posY;
@@ -34,11 +36,15 @@ public class Sprite {
 		this.posY = posY;
 	}
 
+	// Set the size to be scaled by the screen width
+	// I.e. partsOfScreenWidth = 0.1f will scale the size of the sprite down to
+	// be 10% of the screen width.
 	public void setSize(float partOfScreenWidth) {
 		this.width = (int) (screenWidth * partOfScreenWidth);
 		this.height = (int) (width * sprite.getIntrinsicWidth() / sprite.getIntrinsicHeight());
 	}
 
+	// Set the position to be in the center of the screen.
 	public void setPositionCenter() {
 		setPositionCenterX();
 		setPositionCenterY();
@@ -69,7 +75,7 @@ public class Sprite {
 	}
 
 	/**
-	 * Move the icon (dX, dY) in pixels
+	 * Move the sprite (dX, dY) in pixels
 	 */
 	public void move(int dX, int dY) {
 		posX += dX;
@@ -81,7 +87,7 @@ public class Sprite {
 	}
 
 	/**
-	 * Paint this icon on the specified canvas
+	 * Paint this sprite on the specified canvas
 	 */
 	public void draw(Canvas canvas) {
 		// Where to draw the icon
