058badab8862034605c93e3caf640a4b63146f89
Stjerndal <stjernd@kth.se>
2014-12-11 19:09:50 +0100

    Shake trigger completed.

--- Sensorbloom/src/se/tribestar/mobil14/sensorbloom/AccelerometerHandler.java
+++ Sensorbloom/src/se/tribestar/mobil14/sensorbloom/AccelerometerHandler.java
@@ -10,6 +10,14 @@ public class AccelerometerHandler implements SensorEventListener {
 	float accelX;
 	float accelY;
 	float accelZ;
+	double shakeStartTime = Float.MAX_VALUE;
+	boolean shaking;
+	boolean shakeCompleted;
+
+	double checkShakeInterval = 100; // Check for shakes every 100ms
+	double lastUpdate = 0f;
+
+	private static final int SHAKE_THRESHOLD = 400;
 
 	public AccelerometerHandler(Context context) {
 		SensorManager manager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
@@ -23,9 +31,40 @@ public class AccelerometerHandler implements SensorEventListener {
 	}
 
 	public void onSensorChanged(SensorEvent event) {
+		if (!shakeCompleted) {
+			shaking = checkShake(event.values[0], event.values[1], event.values[2]);
+			if (shaking) { // Check if shaked for more than 1 s
+				if (System.currentTimeMillis() - shakeStartTime > 1000) {
+					shakeCompleted = true;
+					V.log("EXPLODE!!!!");
+				}
+			}
+		}
 		accelX = event.values[0];
 		accelY = event.values[1];
 		accelZ = event.values[2];
+
+	}
+
+	private boolean checkShake(float newX, float newY, float newZ) {
+		double curTime = System.currentTimeMillis();
+		if ((curTime - lastUpdate) > 80) {
+			double diffTime = (curTime - lastUpdate);
+			lastUpdate = curTime;
+			double speed = Math.abs(newX + newY + newZ - accelX - accelY - accelZ) / diffTime * 10000;
+			if (speed > SHAKE_THRESHOLD) {
+				V.log("shake!! speed: " + speed);
+				if (!shaking) {
+					shakeStartTime = curTime;
+					V.log("Shake for first time");
+				}
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return shaking;
+		}
 	}
 
 	public float getAccelX() {
